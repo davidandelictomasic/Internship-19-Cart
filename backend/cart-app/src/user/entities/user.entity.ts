9572d1f245026c96902c03162e0c52e5be44f533
davidandelictomasic <david.andelic.tomasic@gmail.com>
2026-03-28 13:39:53 +0100

    Add user CRUD with Prisma

--- backend/cart-app/src/user/entities/user.entity.ts
+++ backend/cart-app/src/user/entities/user.entity.ts
@@ -1,1 +1,21 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class User {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  email: string;
+
+  @ApiProperty()
+  password: string;
+
+  @ApiProperty({ required: false })
+  name: string;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}

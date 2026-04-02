ae3a61f2bb98f97c214aaf202a4ce3904121e49b
davidandelictomasic <david.andelic.tomasic@gmail.com>
2026-04-02 23:29:39 +0200

    Add product grid component to home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import Navbar from '../components/Navbar'
 import BottomNav from '../components/BottomNav'
 import SearchBar from '../components/SearchBar'
+import ProductGrid from '../components/ProductGrid'
 
 function Home() {
   return (
     <div className="pb-16">
       <Navbar />
       <SearchBar />
+      <ProductGrid />
       <BottomNav />
     </div>
   )
